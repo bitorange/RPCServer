80d8005e27d5f1032203371e9861b6ec57c8f407
bitorange <bitorange@foxmail.com>
2015-03-28 10:18:17 +0800

    Initial commit

---
+++ src/main/java/linc/ConnectJDBC.java
@@ -0,0 +1,107 @@
+package linc;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.*;
+
+/**
+ * this class is used to connect the jdbc and execute SQL command.
+ * @author  xwc
+ * @version v1
+ * Created by admin on 2015/3/16.
+ */
+public class ConnectJDBC {
+
+
+     private String msg;
+     private int code;
+
+    /**
+     * the method include register driver,get connection and execute sql, then return resultset
+     *@param sql
+     *@return resultset the Resultset of execute sql command
+     **/
+    public  ResultSet getAndExucuteSQL(String sql,Connection conn){
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            // 注册获得连接 getconnection
+//            conn = JDBCUtils.getConnection();
+            System.out.println(Thread.currentThread().getName() + ": creating statement...");
+            stmt = conn.createStatement();
+        } catch (SQLException e) {
+            code = 1;
+            msg = "there are some problems with the driver.";
+        }
+
+        //执行sql，获取结果集
+        try {
+            rs = stmt.executeQuery(sql);
+            System.out.println(Thread.currentThread().getName() + " executed statement...");
+        } catch (SQLException e) {
+            msg = "";
+            //只取错误信息的后面一段,把冒号分割的第一段省略
+            String msgArray[] = e.getMessage().split(":");
+            for(int i = 1; i <= msgArray.length - 1; i++){
+                msg += msgArray[i];
+            }
+            code = 1 ;
+        }finally {
+            JDBCUtils.releaseAll();
+        }
+        return rs;
+    }
+
+
+    /**
+     * this method transform the resultset into json object ,and return a jsonArray.
+     * @param rs  the resultset
+     * @return JSONArray
+     * */
+    public  JSONObject transformToJsonArray(ResultSet rs) throws JSONException {
+
+        //建立jsonArray数组封装所有的resultset信息
+        JSONArray array = new JSONArray();
+        /*  //wholeArray封装所有包含时间，大小，返回码，返回信息的Json
+        JSONArray wholeArray = null;*/
+        JSONObject wholeJsonObj = null;
+        ResultSetMetaData metaData = null;
+        if(rs !=null) {
+            try {
+                //定义列数
+                int columnCount = 0;
+                //获取列数
+                metaData = rs.getMetaData();
+                columnCount = metaData.getColumnCount();
+
+                //遍历每条数据
+                while (rs.next()) {
+                    //创建json存放resultset
+                    JSONObject jsonObj = new JSONObject();
+                    // 遍历每一列
+                    for (int i = 1; i <= columnCount; i++) {
+                        String columnName = metaData.getColumnLabel(i);
+                        //获得columnName对应的值
+                        String value = rs.getString(columnName);
+                        jsonObj.put(columnName, value);
+                    }
+                    array.put(jsonObj);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        //wholeJsonObj用于存放最终返回数据
+        wholeJsonObj = new  JSONObject();
+        //code为0则正常输出，为10000则异常输出
+        if(code == 1)
+            wholeJsonObj.put("code",code).put("msg",msg);
+            //这里将result,time,code,msg,size数据封装进wholeArray
+        else
+            msg = "success";
+        wholeJsonObj.put("result", array).put("time","20s").put("size","2M").put("code",code).put("msg",msg);
+        return wholeJsonObj;
+    }
+}
---
+++ src/main/java/service/RPCServer.java
@@ -0,0 +1,233 @@
+package service;
+/**
+ * Created by orange on 2015/3/25.
+ */
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.AMQP.BasicProperties;
+import linc.AccountCheck;
+import linc.ConnectJDBC;
+import linc.JDBCUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class RPCServer extends Thread{
+
+    private static final String RPC_QUEUE_NAME = "rpc_queue";
+    public static ConnectionFactory factory;
+    public static Connection connection = null;
+    public static Channel channel = null;
+    public static QueueingConsumer consumer;
+    private static List<Thread> runningThreads = new ArrayList<Thread>();
+    public QueueingConsumer.Delivery delivery;
+    public java.sql.Connection con;
+    public static int JDBCConnections;
+    private static final String conHost="10.4.21.103";
+    private static final String conUsername="wucheng";
+    private static final String conPassword="123456";
+
+
+    static {
+        JDBCConnections = Integer.valueOf(ResourceBundle.getBundle("db").getString("JDBCConnections"));
+    }
+
+    public  RPCServer(QueueingConsumer.Delivery delivery,java.sql.Connection con){
+        this.delivery = delivery;
+        this.con=con;
+    }
+
+    public  RPCServer(){
+
+    }
+
+    public void run()
+    {
+        connectToHive(this.delivery, this.con);
+    }
+
+    /**
+     * 调用函数对用户名密码进行检验
+     * @param name 用户名
+     * @param password 密码
+     * @param con JDBC连接
+     * @return json数据，msg：ok代表登录成功，msg：no代表登录失败，msg：其它代表错误
+     */
+    private String check(String name,String password,java.sql.Connection con){
+        String result= AccountCheck.checkAccount(name, password,con);
+        return "{\"code\": \"10\",\"msg\":\""+result+"\"}";
+    }
+
+    /**
+     * 对SQL语句进行执行并返回结果
+     * @param sql SQL语句
+     * @param con JDBC连接
+     * @return json数据格式的数据
+     */
+    private String sqlExecute(String sql,java.sql.Connection con) {
+        ConnectJDBC conn = new ConnectJDBC();
+        ResultSet rs=null;
+        String response=null;
+        try {
+            rs = conn.getAndExucuteSQL(sql,con);
+        }catch (Exception e){
+            response=e.getMessage().toString();
+            response="{\"code\": \"10\",\"msg\": \""+response+"\"}";
+        }
+
+        /* 将查询出来的结果转换为json数据格式 */
+        JSONObject jsonObject;
+        try {
+            jsonObject = conn.transformToJsonArray(rs);
+            response=jsonObject.toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }finally{
+            JDBCUtils.releaseAll();
+        }
+
+        return response;
+    }
+
+    /**
+     * 获取从服务器消息队列中得到的消息信息，处理并向返回消息队列中返回消息
+     * @param delivery 消息队列中的一个消息
+     * @param con JDBC连接
+     */
+    public void connectToHive(QueueingConsumer.Delivery delivery,java.sql.Connection con){
+
+        String response = null;
+        try {
+            // 获取消息中的信息
+            BasicProperties props = delivery.getProperties();
+            BasicProperties replyProps = new BasicProperties
+                    .Builder()
+                    .correlationId(props.getCorrelationId())
+                    .build();
+
+            try {
+                String message = new String(delivery.getBody(), "UTF-8");
+                JSONObject jsonObject = new JSONObject(message);
+                String service = jsonObject.getString("service");
+
+                if (service.equals("check")) {
+                    /*密码校验*/
+                    String name = jsonObject.getString("name");
+                    String password = jsonObject.getString("password");
+
+                    RPCServer myserver = new RPCServer();
+                    response = myserver.check(name, password,con);
+
+                } else if (service.equals("sqlExecute")) {
+                    /*sql语句执行*/
+                    String sql = jsonObject.getString("sql");
+                    RPCServer myserver = new RPCServer();
+                    response = myserver.sqlExecute(sql,con);
+
+                } else {
+                    /*请求服务出错*/
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                response = "解析出错等错误";
+                response = "{\"code\": \"10\",\"msg\": \"" + response + "\"}";
+            } finally {
+
+                // 将消息返回给信息请求者
+                channel.basicPublish("", props.getReplyTo(), replyProps, response.getBytes("UTF-8"));
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] argv) {
+        ExecutorService pool=null;
+        List<java.sql.Connection> connectionlist=null;
+        try {
+            /* 设置消息队列监控连接 */
+            factory = new ConnectionFactory();
+            factory.setHost(conHost);
+            factory.setUsername(conUsername);
+            factory.setPassword(conPassword);
+
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+
+            //在服务端重启之后，先删除监控队列中在服务器端开启前收到的无用消息
+            channel.queueDelete(RPC_QUEUE_NAME);
+
+            // 声明所监控的消息队列RPC_QUEUE_NAME
+            channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+
+            // 1代表公平转发，不要在同一时间给消费者超过一条信息
+            channel.basicQos(1);
+
+            consumer = new QueueingConsumer(channel);
+            channel.basicConsume(RPC_QUEUE_NAME, false, consumer);
+
+            System.out.println(" Start Server: Monitor Queue");
+
+            //在创建JDBC连接之前注册driver
+            JDBCUtils.loadDriver();
+
+            // 创建JDBCConnections个JDBC连接
+            connectionlist=new ArrayList<java.sql.Connection>();
+            for(int ii=0;ii<JDBCConnections;ii++) {
+                JDBCUtils jd = new JDBCUtils();
+                java.sql.Connection con = jd.getConnection();
+                connectionlist.add(con);
+            }
+
+            // 创建一个可重用固定线程数的线程池
+            pool = Executors.newFixedThreadPool(JDBCConnections);
+            int i=0;
+            while (true) {
+                // 消费者阻塞监听队列
+                QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+                i = i%JDBCConnections;
+                // 创建实现多线程
+                Thread t = new RPCServer(delivery,connectionlist.get(i));
+                i++;
+                // 将线程放入池中进行执行
+                pool.execute(t);
+            }
+
+        }
+        catch  (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            /* release创建的JDBC，线程池，Connection */
+
+            for(int ii = 0; ii < JDBCConnections; ii++) {
+                JDBCUtils.connRelease(connectionlist.get(ii));
+            }
+
+            if(pool!=null) {
+                pool.shutdown();
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                }
+                catch (Exception ignore) {}
+            }
+        }
+    }
+
+
+}
